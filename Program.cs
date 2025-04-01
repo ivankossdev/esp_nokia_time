19de10d06c7da9e18fe90d2b71d04eedcd3626c1
ivankossdev <ivan.koss@yandex.ru>
2025-04-01 14:21:17 +0300

    del

--- Program.cs
+++ Program.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Specialized;
-using System.IO.Ports;
-namespace esp_nokia_time;
+﻿namespace esp_nokia_time;
 
 class Program
 {
     static void Main(string[] args)
     {
         Control.Programm();
-
     }
 }
