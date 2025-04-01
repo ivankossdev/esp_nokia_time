11e59b3ab961555bc6c96865008b09206adcab47
ivankossdev <ivan.koss@yandex.ru>
2025-04-01 09:16:25 +0300

    test set param

--- Program.cs
+++ Program.cs
@@ -8,19 +8,30 @@ class Program
     static void Main(string[] args)
     {
         Commands cmd = new();
-        Console.WriteLine(cmd.SetDate());
-        Console.WriteLine(cmd.SetDay());
-        Console.WriteLine(cmd.SetMonth());
-        Console.WriteLine(cmd.SetYear());
-        // ComPort.Search();
-        // ComPort.Init("/dev/ttyUSB0");
 
-        // bool isOpen = ComPort.Open();
-        // Console.WriteLine($"isOpen {isOpen}");
-        // Thread.Sleep(8000);
-        // ComPort.Write(cmd.SetDay());
-        
-        // isOpen = ComPort.Close();
-        // Console.WriteLine($"isOpen {isOpen}");
+        ComPort.Search();
+        ComPort.Init("/dev/ttyUSB0");
+
+        bool isOpen = ComPort.Open();
+        Console.WriteLine($"isOpen {isOpen}");
+        Thread.Sleep(10000);
+
+        ComPort.Write(cmd.SetDate());
+        Thread.Sleep(2000);
+
+        ComPort.Write(cmd.SetMonth());
+        Thread.Sleep(2000);
+
+        ComPort.Write(cmd.SetYear());
+        Thread.Sleep(2000);
+
+        ComPort.Write(cmd.SetDay());
+        Thread.Sleep(2000);
+
+        ComPort.Write(cmd.SetTime());
+        Thread.Sleep(2000);
+
+        isOpen = ComPort.Close();
+        Console.WriteLine($"isOpen {isOpen}");
     }
 }
